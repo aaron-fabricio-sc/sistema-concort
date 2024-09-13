3f0edccdc05836662f45b99a16792504976515ca
Aaron Fabricio Santa Cruz <aaronfabricio00@gmail.com>
2024-09-13 00:34:44 -0400

    primer guardado

---
+++ routes/admin.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\GrupoController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('', function () {
+    return "asdsa";
+});
